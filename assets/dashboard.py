0e4e63eb032e8398303a2b6195d226631c4e3fb1
Letícia Borges <leticiasousa.borges@gmail.com>
2022-07-18 14:09:37 -0300

    Importação dos pacotes
    importação dos pacotes utilizados para criação do dashboard

---
+++ assets/dashboard.py
@@ -0,0 +1,14 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+import numpy as np
+import pandas as pd
+import json
+
+
